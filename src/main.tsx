01299a1f5c9c149d9e16ed86e80bcd3fd5230864
JohnOmoseni <johnnyomoseni100@gmail.com>
2024-01-08 22:49:19 +0100

    This is an update

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { PuzzleWalletProvider } from '@puzzlehq/sdk';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <div className='h-screen w-screen'>
     <PuzzleWalletProvider
-      dAppName="Where's Alex?"
-      dAppDescription='A friendly wager between friends'
+      dAppName="Where is Daniel?"
+      dAppDescription='inspired by Maury'
       dAppUrl='https://wheresalex.puzzle.online'
       dAppIconURL='https://wheresalex.puzzle.online/alex_head.png'
     >
